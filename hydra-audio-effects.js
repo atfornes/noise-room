47ebfa7a6cebeed9165d2c13be642045ebb465d6
Ámbar Tenorio Fornés <ambar@atenor.io>
2023-10-24 17:25:29 +0200

    fix script links

--- hydra-audio-effects.js
+++ hydra-audio-effects.js
@@ -7,29 +7,29 @@ async function load(){
     window.document.body.appendChild(d);
 
     // copied from index.html
-    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/Audio-Input-Effects@latest/js/effect.js");
-    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/Audio-Input-Effects@latest/js/waveshaper.js");
-    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/Audio-Input-Effects@latest/js/jungle.js");
-    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/Audio-Input-Effects@latest/js/effects.js");
-    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/Audio-Input-Effects@latest/js/pingpong.js");
-    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/Audio-Input-Effects@latest/js/apollo.js");
-    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/Audio-Input-Effects@latest/js/cursorControls.js");
+    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/noise-room@latest/js/effect.js");
+    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/noise-room@latest/js/waveshaper.js");
+    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/noise-room@latest/js/jungle.js");
+    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/noise-room@latest/js/effects.js");
+    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/noise-room@latest/js/pingpong.js");
+    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/noise-room@latest/js/apollo.js");
+    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/noise-room@latest/js/cursorControls.js");
 
     // Visualizer stuff -->
-    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/Audio-Input-Effects@latest/js/visualizer/events.js");
+    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/noise-room@latest/js/visualizer/events.js");
 
     // WebGL stuff -->
-    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/Audio-Input-Effects@latest/js/visualizer/base.js");
-    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/Audio-Input-Effects@latest/js/visualizer/cameracontroller.js");
+    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/noise-room@latest/js/visualizer/base.js");
+    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/noise-room@latest/js/visualizer/cameracontroller.js");
 
     // TODO(kbr): remove this dependency -->
-    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/Audio-Input-Effects@latest/js/visualizer/matrix4x4.js");
+    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/noise-room@latest/js/visualizer/matrix4x4.js");
 
     // Visualizer GL library -->
-    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/Audio-Input-Effects@latest/js/visualizer/visualizer.js");
-    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/Audio-Input-Effects@latest/js/visualizer/shader.js");
+    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/noise-room@latest/js/visualizer/visualizer.js");
+    await loadScript("https://cdn.jsdelivr.net/gh/atfornes/noise-room@latest/js/visualizer/shader.js");
 
-    const resp = await fetch("https://cdn.jsdelivr.net/gh/atfornes/Audio-Input-Effects@latest/index.html");
+    const resp = await fetch("https://cdn.jsdelivr.net/gh/atfornes/noise-room@latest/index.html");
     const html = await resp.text();
     document.body.insertAdjacentHTML("beforeend", html);
     }
