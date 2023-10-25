6e47181eb20318a831636fdc246aaefcb4de8db8
Ámbar Tenorio Fornés <ambar@atenor.io>
2023-10-25 17:06:54 +0200

    fix z-index

--- addIframe.js
+++ addIframe.js
@@ -10,7 +10,7 @@ async function load(){
     iframe.id="audioEffects"
     iframe.src="https://atfornes.github.io/noise-room"
     iframe.allow="microphone"
-    iframe.style="position: absolute; height: 100%; right: 0; top: 50px"
+    iframe.style="position: absolute; height: 100%; right: 0; top: 50px; z-index=10"
     document.body.appendChild(iframe);
 
     let d = document.createElement("div");
