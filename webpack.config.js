3e8398ea70c85a9f82d1f1c4aff3b878fd095a42
Ámbar Tenorio Fornés <ambar@atenor.io>
2023-10-25 11:46:25 +0200

    configure webpack to produce a single file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './hydra-audio-effects.js',
+  plugins: [
+  new webpack.optimize.LimitChunkCountPlugin({
+    maxChunks: 1
+  })],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
